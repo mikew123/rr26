a13f2a37d9cfe7bf1714850660328af743d5d3d5
mikew123 <mikew123@gmail.com>
2026-04-18 20:54:25 -0500

    Attempt to fix angular lidar scan motion error. Need to develop a metric to measure fix is good

--- src/rr26_stuff/rr26_stuff/rr26_scan_fix_node.py
+++ src/rr26_stuff/rr26_stuff/rr26_scan_fix_node.py
@@ -6,6 +6,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 class rr26ScanFixNode(Node):
@@ -16,15 +17,45 @@ class rr26ScanFixNode(Node):
     def __init__(self):
         super().__init__('rr26_scan_fix_node')
 
+        self.cmd_vel_subscription = self.create_subscription(Twist, '/cmd_vel', 
+                                                             self.cmd_vel_callback, 10)
         self.scan_msg_subscriber = self.create_subscription( LaserScan, 'scan', 
                                                             self.scan_msg_callback, 10)
         self.scan_fix_msg_publisher = self.create_publisher(LaserScan, 'scan_fix', 10)
+
+        self.timer = self.create_timer(1.0, self.timer_callback)
 
         self.get_logger().info(f"rr26ScanFixNode: Started")
 
     def cleanup(self) :
         self.scan_fix_msg_publisher.destroy()
 
+
+    # robot velocities used for motion compensation of Lidar scan data
+    linX:float = 0.0
+    angZ:float = 0.0
+
+    def timer_callback(self) -> None :
+        """
+        Clears the current velocities when there is no /cmd_vel data
+        """
+        linX = 0.0
+        angZ = 0.0
+
+
+    def cmd_vel_callback(self, msg:Twist) -> None :
+        """
+        Extracts the current linear and angular velocities
+        which are used to "fix" the Lidar scan data
+        The velocities are saved in global variables 
+        The saved velocities are cleared to 0.0 if the timer times out
+        """
+
+        self.timer.reset()
+
+        self.linX = msg.linear.x
+        self.angZ = msg.angular.z
+    
     def scan_msg_callback(self, msg:LaserScan) -> None :
         """
         Processes the raw Lidar scan data to fix the rotational
@@ -33,12 +64,32 @@ class rr26ScanFixNode(Node):
         A corrected /scan_fix message is published
         """
 
+        linx = self.linX
+        angZ = self.angZ
+
         # copy all the original scan message variables before fixing
         msg_fix = msg
-        # TODO: fix distortions
         
+        # extract the lidar scan parameters
+        rmin = msg.range_min
+        amin = msg.angle_min
+        amax = msg.angle_max
+        ainc = msg.angle_increment
+        rays = msg.ranges
+        nrays = len(rays)
+        stime = msg.scan_time
+
+        # calc the angular error compensation
+        angErr = self.angZ * stime
+        amax += angErr
+        ainc = (amax - amin) / nrays
+        msg.angle_max = amax
+        msg.angle_increment = ainc
+
         self.scan_fix_msg_publisher.publish(msg_fix)
 
+        if angZ != 0.0 :
+            self.get_logger().info(f"scan_msg_callback: {angZ=} {stime=} {angErr=} {ainc=} {amin=} {amax=} {nrays=}")
 
 def main(args=None):
     rclpy.init(args=args)
